16dc5bc05c1f4eac56bae2382c6f2b4ce450ef27
Yvan Kouematchoua <yvan.kouematchoua@rwth-aachen.de>
2025-03-13 13:56:20 -0300

    Brute force algorithm with periodic boundaries. Warning: different results than CIM with same conditions.

--- brute_force.py
+++ brute_force.py
@@ -39,9 +39,40 @@ class BruteForce:
 
         for p_1 in range(number_particles):
             neighbor_index_list = []
+            periodic_particles = []
+
+            if self.periodic:  # TODO: check whether square is a box from 0 to L
+                if self.l - particles[p_1][0] <= radius_interaction + max_radius:
+                    periodic_particles.append((particles[p_1][0] - self.l, particles[p_1][1]))
+
+                    if self.l - particles[p_1][1] <= radius_interaction + max_radius:
+                        periodic_particles.append((particles[p_1][0] - self.l, particles[p_1][1] - self.l))
+                    if particles[p_1][1] <= radius_interaction + max_radius:
+                        periodic_particles.append((particles[p_1][0] - self.l, particles[p_1][1] + self.l))
+
+                if self.l - particles[p_1][1] <= radius_interaction + max_radius:
+                    periodic_particles.append((particles[p_1][0], particles[p_1][1] - self.l))
+
+                if particles[p_1][0] <= radius_interaction + max_radius:
+                    periodic_particles.append((particles[p_1][0] + self.l, particles[p_1][1]))
+
+                    if self.l - particles[p_1][1] <= radius_interaction + max_radius:
+                        periodic_particles.append((particles[p_1][0] + self.l, particles[p_1][1] - self.l))
+                    if particles[p_1][1] <= radius_interaction + max_radius:
+                        periodic_particles.append((particles[p_1][0] + self.l, particles[p_1][1] + self.l))
+
+                if particles[p_1][1] <= radius_interaction + max_radius:
+                    periodic_particles.append((particles[p_1][0], particles[p_1][1] + self.l))
+
             for p_2 in range(number_particles):
-                if p_1 != p_2 and m.dist(particles[p_1], particles[p_2]) <= radius_interaction + 2 * max_radius:  # TODO: check radius correct
-                    neighbor_index_list.append(p_2)
+                if p_1 != p_2:  # TODO: check radius correct
+                    if m.dist(particles[p_1], particles[p_2]) <= radius_interaction + 2 * max_radius:
+                        neighbor_index_list.append(p_2)
+                    else:
+                        for particle in periodic_particles:
+                            if m.dist(particle, particles[p_2]) <= radius_interaction + 2 * max_radius:
+                                neighbor_index_list.append(p_2)
+
             output_distances[p_1] = neighbor_index_list
             for idx in neighbor_index_list:
                 result[p_1].add(idx)
